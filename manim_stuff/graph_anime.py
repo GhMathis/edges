2b0cdc55b0554f7df2cb7989a3831e0cd258a3f7
Mathis.g <mathisgheno@gmail.com>
2023-05-10 21:37:38 -0400

    :bookmark: manim v 0.1, spectral visu of communicability

--- manim_stuff/graph_anime.py
+++ manim_stuff/graph_anime.py
@@ -6,7 +6,9 @@ Created on Tue May  9 17:52:49 2023
 """
 
 from manim import *
+from manim.utils.color import Colors
 import numpy as np
+import networkx as nx
 
 class Networks(Scene):
     
@@ -23,23 +25,52 @@ class Networks(Scene):
         adj_arr[ID] = 0
         self.edges_temp = np.where(adj_arr == 1)
         self.edges = [(e1,e2) for e1,e2 in zip(self.edges_temp[0],self.edges_temp[1])]
+    
+    def egde_color(self,decompo_arr):
+        ID = np.triu_indices(decompo_arr.shape[0],1)
+        decompo_arr[ID] = 0
+        pos_edges =  np.where(decompo_arr > 0)
+        neg_edges =  np.where(decompo_arr < 0)
+        egde_col_pos = {(e1,e2):{"stroke_color": Colors.blue.value} for e1,e2 in zip(pos_edges[0], pos_edges[1])}
+        egde_col_pos2 = {(e1,e2):{"stroke_color": Colors.red.value} for e1,e2 in zip(neg_edges[0], neg_edges[1])}
+        egde_col_pos = egde_col_pos| egde_col_pos2
         
+        return(egde_col_pos)
+    
     def construct(self):
         adj_arr = np.array([[0, 1, 1, 1, 1],
                             [1, 0, 1, 0, 1],
                             [0, 1, 0, 1, 0],
                             [1, 0, 1, 0, 1],
                             [1, 1, 0, 1, 0]])
-       
+        spectra = np.linalg.eig(adj_arr)
         ##### Setup the data for visualisations
         self.adj_to_list(adj_arr)
         
         
-        meta_ntw = Graph(vertices = self.vertices,
-                         edges =self.edges )
+        G_decomposition = lambda val, vec: np.dot(vec.T, vec)*np.exp(val)
+        list_g_decomp = [G_decomposition(val, vec[np.newaxis]) for val, vec
+                 in zip(spectra[0], spectra[1])]
         
-        self.play(LaggedStart(Create(meta_ntw),  run_time=3))
+
+        edge_col = self.egde_color(list_g_decomp[0])
+        
+        ##### Object creation
+        lyt = nx.bipartite_layout(self.vertices, self.vertices[:2])
+        
+        meta_ntw = [Graph(vertices = self.vertices,
+                         edges = self.edges,
+                         labels =True,
+                         layout = "planar",
+                         edge_config = self.egde_color(g_decomp)) for g_decomp
+                    in list_g_decomp]
+
+        r1 = VGroup(*meta_ntw[:3]).arrange()
+        r2 = VGroup(*meta_ntw[3:]).arrange()
+        
+        self.play(LaggedStart(Create(VGroup(r1,r2).arrange(direction=DOWN)),
+                              run_time=3))
         self.wait(2)
-        
+    
         
 #python -m manim -pql graph_anime.py Networks
