d1c24092e3575af52d1d878172822b61fd3d5b87
Mathis.g <mathisgheno@gmail.com>
2023-05-13 11:26:32 -0400

    :mag: full data communicability

--- R/communicability_parallel_process.R
+++ R/communicability_parallel_process.R
@@ -1,20 +1,20 @@
 library(tidyverse)
 library(doParallel) # for parallel processing
 library(parallel) # to detect core
-
-#setwd("2*")
-setwd("~/Fac/Master_Rennes/stage1/edges")
-source("exploratory_code/functions.R")
+getwd()
+#setwd("~/lustre06/project/6002172/edges")
+#setwd("~/Fac/Master_Rennes/stage1/edges")
+source("R/functions.R")
 
 clover = read.csv("data/clover.csv", stringsAsFactors = T)
 trefle = read.csv("data/trefle.csv", stringsAsFactors = T)
 
 ##### 1000 interaction for testing /!\to be removed/!\
-set.seed(1000)
-ID = sample(nrow(clover), 1000)
-clover = clover[ID, ]
- 
-trefle = trefle[trefle$host %in% clover$Host & trefle$virus %in% clover$Virus ,]
+# set.seed(1000)
+# ID = sample(nrow(clover), 1000)
+# clover = clover[ID, ]
+# 
+# trefle = trefle[trefle$host %in% clover$Host & trefle$virus %in% clover$Virus ,]
 #####
 
 ID_trefle = which( paste(trefle$virus, trefle$host) %in% 
@@ -29,19 +29,23 @@ G_clover = communicability(uni_ntw_clover)
 ntw_base = list(uni_ntw_clover = uni_ntw_clover, uni_ntw_trefle = uni_ntw_trefle,
                 G_trefle = G_trefle, G_clover = G_clover)
 
-
-communi.func<- function(virus, host, row_ID, arg){
+communi.func<- function(virus,host,row_ID,arg){
+  
+  print(row_ID)
+  
   with(arg,{
     
     ID_virus = which(row.names(uni_ntw_clover) == virus)  
     ID_host = which(colnames(uni_ntw_clover) == host) 
-    
     uni_ntw_trefle[ID_virus,ID_host] = 0 # change the interaction (perturbation == zeta)
     uni_ntw_trefle[ID_host,ID_virus] = 0 
     uni_ntw_clover[ID_virus,ID_host] = 0
     uni_ntw_clover[ID_host,ID_virus] = 0
+
     G_zeta_trefle = communicability(uni_ntw_trefle)
+
     G_zeta_clover = communicability(uni_ntw_clover)
+    
     uni_ntw_trefle[ID_virus,ID_host] = 1  # remove the change
     uni_ntw_trefle[ID_host,ID_virus] = 1 
     uni_ntw_clover[ID_virus,ID_host] = 1
@@ -96,7 +100,9 @@ communi.func<- function(virus, host, row_ID, arg){
                    f_norm_G_clover_nrmlz = f_norm_G_clover_nrmlz,
                    G_pq_trefle_nrmlz = G_pq_trefle_nrmlz,
                    G_pq_clover_nrmlz = G_pq_clover_nrmlz)
+
     row.names(df) = row_ID
+    
     return(df)
     
   })
@@ -105,14 +111,13 @@ communi.func<- function(virus, host, row_ID, arg){
 n_iteration = nrow(shared_asso)
 
 ncores =detectCores()
-registerDoParallel(cores=ncores-1)# Shows the number of Parallel Workers to be used
+print(ncores)
+registerDoParallel(cores=(ncores-1))# Shows the number of Parallel Workers to be used
 
-#getDoParWorkers()# number of actual workers
+getDoParWorkers()# number of actual workers
 
 
-communicability_df = foreach(virus = shared_asso$virus, host = shared_asso$host, row_ID = row.names(shared_asso),
-               arg = lapply(1:4, function (x) ntw_base),
-        .combine = rbind, .verbose = T) %dopar%{communi.func(virus = virus, host = host, row_ID = row_ID, arg=arg)}
+communicability_df = foreach(virus = shared_asso$virus, host = shared_asso$host, row_ID = row.names(shared_asso), arg = lapply(1:n_iteration, function (x) ntw_base),.combine = rbind, .verbose = T) %dopar%{communi.func(virus = virus, host = host, row_ID = row_ID, arg=arg)}
 
 write.csv(communicability_df, "output/communicability_df.csv")
 
