3aca5ecc741d3e5af8f7757b7e07bf5bd384812e
Mathis.g <mathisgheno@gmail.com>
2023-06-13 20:40:11 -0400

    :bookmark: manim v 1.0

--- manim_stuff/graph_anime.py
+++ manim_stuff/graph_anime.py
@@ -144,25 +144,26 @@ class Networks(Scene):
 
         move_animations= {}
         print(meta_ntw.edges.items())
-        def all_arrows(lright, lend):
+        def all_arrows(lright, lend, k):
             arrows = []
             for r,l in zip(lright,lleft):
-               arrows.append(Arrow(start = r + tracker.get_value()*(l-r)*
+               arrows.append(Line(start = r + tracker.get_value()*(l-r)*
                               (np.sqrt(tracker.get_value())),
                               end = r + tracker.get_value()*(l-r)*
                               (2-tracker.get_value()),
-                              color = GREEN, buff = 0,
-                          max_stroke_width_to_length_ratio = 20,
-                          max_tip_length_to_length_ratio=20)
+                              color = GREEN, buff = 0, tip_length = 0.3 *k
+                          # max_stroke_width_to_length_ratio = 20,
+                          # max_tip_length_to_length_ratio=20*k,
+                          ).add_tip()
                              )
             return VGroup(*arrows)
         
         self.play(LaggedStart(Create(r1),
                               run_time=3))
-        self.wait(2)
+        self.wait(1)
     
-        for step in range(2):
-            
+        for step in range(4):
+            tracker = ValueTracker(0.009)
             start = start_temp
             start_temp = []
             move_animation = []
@@ -172,47 +173,16 @@ class Networks(Scene):
             
                 if v[0] in start and v[0] in [0,1,2]:
 
-                    #move_animation = MoveAlongPath(Dot(color=BLUE), l)#.reverse_points()
-                    # move_animation =  Dot(color=YELLOW).add_updater(
-                    #     lambda arrow : arrow.move_to(l.number_to_point(tracker.getvalue()))
-                    #     ).update()
                     lleft.append(l.get_end())
                     lright.append(l.get_start())
-
-                #     move_animation.append(Arrow(start = lstart, end = p, color = GREEN, buff = 0,
-                #               max_stroke_width_to_length_ratio = 20,
-                #               max_tip_length_to_length_ratio=20).add_updater(
-                #                   lambda arrow : trackfunc_toleft(arrow,l)).update())
                 
                 elif v[1] in start and v[1] in [3,4,5,6]:
                     print(["v:", v, " start : " ,start, "step : ", step ])
                     lleft.append(l.get_start())
                     lright.append(l.get_end())
-                    # move_animation.append(always_redraw(
-                    #     lambda: Arrow(start = lstart + tracker.get_value()*(lend-lstart)*
-                    #                   (np.sqrt(tracker.get_value())),
-                    #                   end = lstart + tracker.get_value()*(lend-lstart)*
-                    #                   (2-tracker.get_value()),
-                    #                   color = GREEN, buff = 0,
-                    #               max_stroke_width_to_length_ratio = 20,
-                    #               max_tip_length_to_length_ratio=20)
-                    #     )
-                    # )
+
                 draw_arrows = always_redraw(
-                    lambda: all_arrows(lright, lleft))
-                    
-                #     print(["v:", v, " start : " ,start, "step : ", step ])
-                #     #move_animation = MoveAlongPath(Dot(color=BLUE), l.reverse_points())
-                #     # move_animation =  Dot(color=YELLOW).add_updater(
-                #     #     lambda arrow : arrow.move_to(l.number_to_point(tracker.getvalue()))
-                #     #     ).update()
-                #     lend= l.get_end()
-                #     lstart = l.get_start()
-                #     p = lstart+0.01*(lend-lstart)
-                #     move_animation.append(Arrow(start = lstart, end = p, color = GREEN, buff = 0,
-                #               max_stroke_width_to_length_ratio = 20,
-                #               max_tip_length_to_length_ratio=20).add_updater(
-                #                   lambda arrow : trackfunc_toright(arrow,l)).update())                 
+                    lambda: all_arrows(lright, lleft, 1/math.factorial(step+1)))
 
                 print([" start : " ,start, "step : ", step ])
                 if v[0] in start or v[1] in start:
@@ -221,88 +191,10 @@ class Networks(Scene):
                     if v[1] not in start:
                         start_temp.append(v[1])
             self.play(Create(draw_arrows))
-            self.wait(2)
-            self.play(tracker.animate.set_value(1), run_time=10, rate_func = linear)
+            self.play(tracker.animate.set_value(1), run_time=2, rate_func = linear)
             #move_sequence = AnimationGroup(*move_animations)
             #move_animations[f"{step}"]= VGroup(*move_animation)
             #self.play(move_sequence, run_time=10, rate_func=linear)
         
+# python -m manim -pql graph_anime.py Networks     
         
-        
-        #for arrows in move_animations.values():
-
-           
-            # self.play(UpdateFromAlphaFunc(
-            #     tracker, 
-            #     lambda arrows, alpha: arrows.set_value(alpha),
-            #     run_time=10
-            #     )
-            # )
-            self.wait(2)
-  
-# def right_to_left(l, t):
-#     x = l.get_slope()  
-#     b = l.get_start_and_end()[1]  
-#     return(t*x+b[1])  
-
-# def move_arrow(arrow, t):
-#     right_to_left()
-#     b = l.get_start_and_end()[1]  
-#     return(x*b[0]+b[1]) 
-# def left_to_right(arrow, l, t):
-#     x = l.get_slope()  
-#     b = l.get_start_and_end()[1]  
-#     a.put_start_and_end_on
-#     return(x*b[0]+b[1]) 
-# #python -m manim -pql graph_anime.py Networks
-
-# class basis2( Scene ):
-#     def construct( self ):
-
-#         radius = 4
-#         axes = Axes( x_range = [0, 1, 1], y_range = [0, 1, 1], x_length = radius, y_length = radius,
-#                         axis_config = {"include_tip": True, "numbers_to_exclude": [0]} ).add_coordinates()
-
-#         origin = axes.coords_to_point( 0, 0 )
-#         e1dir = RIGHT
-#         e2dir = UP
-
-#         x1 = origin + radius * e1dir
-#         x2 = origin + radius * e2dir
-
-#         r = lambda t: origin + radius * e1dir * np.cos( t * 0.5 * PI ) + radius * e2dir * np.sin( t * 0.5 * PI )
-#         tv = lambda t: - e1dir * np.sin( t * 0.5 * PI ) + e2dir * np.cos( t * 0.5 * PI )
-
-#         def rmove(a, t):
-#             p = r(t)
-#             d = (p - origin)/4
-#             a.put_start_and_end_on( p, p + d )
-
-#         def tmove(a, t):
-#             p = r(t)
-#             d = tv(t)
-#             a.put_start_and_end_on( p, p + d )
-
-#         def u1(mob):
-#             t = t_parameter.get_value()
-#             rmove( mob, t )
-
-#         def u2(mob):
-#             t = t_parameter.get_value()
-#             tmove( mob, t )
-
-#         t_parameter = ValueTracker(0)
-#         e1 = Arrow( start = x1, end = x1 + e1dir, color = GREEN, buff = 0 ).add_updater(u1).update()
-#         e2 = Arrow( start = x1, end = x1 + e2dir, color = GREEN, buff = 0 ).add_updater(u2).update()
-
-#         g1 = ParametricFunction( r,
-#                                     t_range=[0, 1],
-#                                     scaling=axes.x_axis.scaling, color=YELLOW )
-
-#         self.add( VGroup( axes, g1, e1, e2 ) )
-#         self.wait( 1 )
-
-#         self.play( UpdateFromAlphaFunc( t_parameter, 
-#                                         lambda mob, alpha: mob.set_value( alpha ) ),
-#                                         run_time=6 )
-#         self.wait( 1 )
