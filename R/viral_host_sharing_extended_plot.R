9baad1f2f63dba806e9f90fb356e3fe50e6b2042
Mathis.g <mathisgheno@gmail.com>
2023-06-30 11:32:52 -0400

    :wrench: small fix

--- R/viral_host_sharing_extended_plot.R
+++ R/viral_host_sharing_extended_plot.R
@@ -1,15 +1,16 @@
+viral_host_sharing_extended_df = read.csv("output/viral_host_sharing_extended_df.csv", header = T)
 str(viral_host_sharing_extended_df)
 host_recap = viral_host_sharing_extended_df%>%
   select(c("HostOrder_zeta","HostOrder","host", "score_host_group","deepness","X"))%>%
   group_by(HostOrder,HostOrder_zeta, deepness)%>%
   summarise(mean_score = mean(score_host_group, na.rm=TRUE),
             sd_score = sd(score_host_group, na.rm=TRUE))
-
+viral_host_sharing_extended_df$score_host_group
 ggplot(host_recap)+
-  geom_point(aes(HostOrder,HostOrder_zeta , col = mean_score),size = 5)+
+  geom_point(aes(HostOrder_zeta,HostOrder , col = mean_score),size = 10,shape = 15)+
   facet_wrap(~deepness)
 
-
+max(host_recap$mean_score)
 temp = viral_host_sharing_extended_df%>%
   select(c("HostOrder_zeta","HostOrder","host", "score_host_group","deepness","X"))%>%
   filter(HostOrder_zeta ==HostOrder )
